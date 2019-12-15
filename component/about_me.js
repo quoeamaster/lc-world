66f9fdfcf44707d2682bb55e29eae47008271036
que master <quoeamaster@gmail.com>
2019-12-15 18:10:27 +0800

    2019-12-15c

--- component/about_me.js
+++ component/about_me.js
@@ -1,8 +1,27 @@
 
 Vue.component('about-me', {
+  props: ['about'],
   template: `
-<div>
-
+<div id="_about_" class="am-core">
+    <div class="am-content-container">
+        <div class="am-h3">{{getCaption()}}</div>
+        <div class="am-h4">{{getContent()}}</div>
+    </div>
 </div>
-  `
+  `,
+  methods: {
+    getCaption: function () {
+      if (this.about) {
+        return this.about.caption;
+      }
+      return '';
+    },
+    getContent: function () {
+      if (this.about) {
+        return this.about.content;
+      }
+      return '';
+    }
+
+  }
 });
